3693cd2e27804af0df518a7181aa7d4dadcf3f33
Gabriel Erich Franco <gabi10.franco@gmail.com>
2026-04-03 12:14:00 -0300

    Melhora metodo soma com mensagem

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,8 @@
 public class Calculadora {
 	public static int somar(int a, int b){
 		return a + b;
+		System.out.println("Realizando soma...");
+		return resultado;
 	}
 
 	public static void main(String[] args){
